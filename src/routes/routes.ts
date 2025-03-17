01b4398fdce133784ef7e6f30d618e3b117e23ee
Gabriela Ustariz <gabrielaustariz@gmail.com>
2025-03-17 06:05:56 -0400

    feat: add env variables

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
-export const BASE_API_URL = 'http://localhost:8069';
-export const BASE_CLIENT_URL = 'http://localhost:3000';
+export const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BASE_CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL;
 
 // API ROUTES
 export const EXPERIMENT_URL = `/experiments/`;
